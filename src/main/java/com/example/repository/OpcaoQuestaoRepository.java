2f9c236222084cdb0339fab62cc01d6229c14db4
p_din <p_diniz_1991@hotmail.com>
2025-10-16 22:29:14 -0300

    BANCO DE DADOS ADICIONADO

--- src/main/java/com/example/repository/OpcaoQuestaoRepository.java
+++ src/main/java/com/example/repository/OpcaoQuestaoRepository.java
@@ -2,8 +2,10 @@ package com.example.repository;
 
 import com.example.model.OpcaoQuestao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository; 
 
+@Repository 
 public interface OpcaoQuestaoRepository extends JpaRepository<OpcaoQuestao, Integer> {
     
-   
+  
 }
